436aefffd2a07011034a1778f975d31b2f7f75f2
Ankur Solanki <ankursolanki1537@gmail.com>
2023-04-13 16:22:04 +0530

    time queries left

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -10,6 +10,189 @@ function allPresent(requiredProperties,givenProperties){
     return requiredProperties.every((property) => property in givenProperties)
 }
 
+function getTotalUsers(){
+    return new Promise((resolve,reject)=>{
+        sqlConnection.query('select count(*) as count from userdata',(err,row,field)=>{
+            if(!err){
+                const count = row[0].count
+                resolve({totalusers:count})
+            }else{
+                console.log(err)
+                reject(err)
+            }
+        })
+    })
+}
+
+function getTotalSellers(){
+    return new Promise((resolve,reject)=>{
+        sqlConnection.query('select count(*) as count from userdata where category = "seller" or category = "both"',(err,row,field)=>{
+            if(!err){
+                const count = row[0].count
+                resolve({totalsellers:count})
+            }else{
+                console.log(err)
+                reject({error:err})
+            }
+        })
+    })
+}
+
+function getTotalBuyer(req,res){
+    return new Promise((resolve,reject)=>{
+        sqlConnection.query('select count(*) as count from userdata where category = "buyer"',(err,row,field)=>{
+            if(!err){
+                const count = row[0].count
+                resolve({totalbuyers:count})
+            }else{
+                console.log(err)
+                reject({error:err})
+            }
+        })
+    })
+}
+
+function notLoggedAferSignup(req,res){
+    return new Promise((resolve,reject)=>{
+        sqlConnection.query('select count(*) as count from userdata where username not in (select username from logindata)',(err,row,field)=>{
+            if(!err){
+                const count = row[0].count
+                resolve({notloggedaftersignup:count})
+            }else{
+                console.log(err)
+                reject({error:err})
+            }
+        })
+    })
+}
+
+async function mostSoldFruit(){
+        return db.collection('sellingdata').get().then((querySnapshot) => {
+            if (!querySnapshot.empty) {
+    
+              const allFruitsData = []
+    
+              querySnapshot.forEach((doc) => {
+                const data = doc.data(); 
+                allFruitsData.push(data)
+              });
+    
+              let fruit_totals = {}
+    
+                for(let fruit of allFruitsData){
+                    const fruitName = fruit.fruit
+                    const quantity = fruit.quantity
+    
+                    console.log(fruitName+" "+quantity)
+                    if(fruitName in fruit_totals){
+                        fruit_totals[fruitName] = fruit_totals[fruitName]+quantity
+                    }else{
+                        fruit_totals[fruitName] = quantity
+                    }
+                }
+    
+                let maxFruit = "";
+                let maxQuantity = 0;
+    
+                for (const fruit in fruit_totals) {
+                    if (fruit_totals[fruit] > maxQuantity) {
+                        maxQuantity = fruit_totals[fruit];
+                        maxFruit = fruit;
+                    }
+                }
+    
+                return {fruit: maxFruit};
+                } else {
+                    return {message:'No data found'}
+                }
+            }).catch(e => {
+                console.log(e)
+                return {error:e}
+            })
+    
+}
+
+async function getAllFruitData(){
+    return 
+}
+
+async function unsoldFruit(){
+        return db.collection('sellingdata').get().then(async (querySnapshot) => {
+            if (!querySnapshot.empty) {
+
+            const allFruitsData = []
+
+            querySnapshot.forEach((doc) => {
+                const data = doc.data(); 
+                allFruitsData.push(data)
+            });
+
+            let fruits = new Set()
+
+            for(let fruit of allFruitsData){
+                const fruitName = fruit.fruit
+
+                if(!fruits.has(fruitName)){
+                    fruits.add(fruitName)
+                }
+            }
+
+            const unsold = await new Promise((resolve,reject)=>{
+                sqlConnection.query('select name from fruitdata',(err,row,field)=>{
+                    if(!err){
+                        let fruitdata = [...row]
+                        let unsoldArray = []
+                        for(let fruit of fruitdata){
+                            if(!fruits.has(fruit.name)){
+                                unsoldArray.push(fruit.name)
+                            }
+                        }
+        
+                        resolve({unsold: unsoldArray})
+                    }else{
+                        reject({error:err})
+                    }
+                })
+            })
+
+            return unsold
+            
+            } else {
+                return {message:'No data found'}
+            }
+        }).catch(e => {
+            console.log(e)
+            return {error:e}
+        })
+
+}
+
+function userFruitSellers(username){
+    return (new Promise((resolve,reject)=>{
+        sqlConnection.query('select * from fruitdata where username = ?',[username],(err,row,field)=>{
+            if(!err){
+                if(row.length !== 0){
+                    resolve(row)
+                }else{
+                    reject({message:"no fruits"})
+                }
+            }else{
+                console.log(err)
+                reject({error:err})
+            }
+        })
+    })).then((res)=>{
+        const arr = [...res]
+        let fruitnames = []
+        for(let fruit of arr){
+            fruitnames.push(fruit.name)
+        }
+        return {fruits: fruitnames}
+    }).catch(e=>{return {error:e}})
+}
+
+
+
 
 // controllers
 
@@ -120,4 +303,14 @@ async function addMoney(req,res){
     
 }
 
-module.exports = {signup,login,addMoney}
+async function getData(req,res){
+
+    // Number of total users
+    //const result = await mostSoldFruit()
+    // const result1 = await userFruitSellers("basanti")
+    const result1 = await unsoldFruit()
+    res.json(result1)
+}
+
+
+module.exports = {signup,login,addMoney, getData}
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const UserRouter = express.Router()
 
-const {signup,login,addMoney} = require('../controllers/user.controller')
+const {signup,login,addMoney,getData} = require('../controllers/user.controller')
 const {validateAuthentication} = require('../middlewares/auth.middleware')
 
 UserRouter.post('/signup',signup)
 UserRouter.post('/login',login)
 UserRouter.post('/addmoney',validateAuthentication,addMoney)
+UserRouter.get('/data',validateAuthentication,getData)
 
 module.exports = UserRouter
